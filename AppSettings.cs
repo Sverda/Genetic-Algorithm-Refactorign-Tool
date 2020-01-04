d6d86053b47be3b9321f32ae1099d257cc57dbff
Damian Ubowski <sverda300@gmail.com>
2020-01-04 18:18:44 +0100

    [ExtractMI] Extract 'MaintainabilityIndex' from generated project xml

--- AppSettings.cs
+++ AppSettings.cs
@@ -3,18 +3,36 @@ using System.Configuration;
 
 namespace GenSharp
 {
-    class AppSettings
+    class MISettings
     {
         private const string _miKey = "MaintainabilityIndexToolPath";
+        private const string _csprojPath = "csprojPath";
+        private const string _targetMethodName = "targetMethodName";
 
         public string MetricsExecutablePath { get; }
 
-        public AppSettings()
+        public string TargetProjectPath { get; }
+
+        public string TargetMethodName { get; }
+
+        public MISettings()
         {
             MetricsExecutablePath = ConfigurationManager.AppSettings[_miKey];
             if (string.IsNullOrEmpty(MetricsExecutablePath))
             {
                 throw new ArgumentNullException(nameof(MetricsExecutablePath));
+            }
+
+            TargetProjectPath = ConfigurationManager.AppSettings[_csprojPath];
+            if (string.IsNullOrEmpty(TargetProjectPath))
+            {
+                throw new ArgumentNullException(nameof(TargetProjectPath));
+            }
+
+            TargetMethodName = ConfigurationManager.AppSettings[_targetMethodName];
+            if (string.IsNullOrEmpty(TargetMethodName))
+            {
+                throw new ArgumentNullException(nameof(TargetMethodName));
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
-﻿namespace GenSharp
+﻿using System.Diagnostics;
+using System.Xml;
+
+namespace GenSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = new MISettings().MetricsExecutablePath,
+                    Arguments = $"/project:{new MISettings().TargetProjectPath} /quiet",
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                },
+                EnableRaisingEvents = true
+            };
+            process.OutputDataReceived += (sender, e) =>
+            {
+                var xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(e.Data);
+                var metricNode = xmlDoc.SelectSingleNode($"number(//Method[contains(@Name, '{new MISettings().TargetMethodName}')]/Metrics/Metric[@Name = 'MaintainabilityIndex'])");
+                var miValue = metricNode.Attributes["Value"].Value;
+            };
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
         }
     }
 }
