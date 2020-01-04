14a3c67455d1e60846ad7370bad1547bd8ab080a
Damian Ubowski <sverda300@gmail.com>
2020-01-04 18:48:50 +0100

    [RefactorExtractMI] Divide 'Main' method into smaller pieces

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Text;
 using System.Xml;
 
 namespace GenSharp
@@ -6,6 +8,25 @@ namespace GenSharp
     class Program
     {
         static void Main(string[] args)
+        {
+            Console.WriteLine(ComputeIndex());
+        }
+
+        private static int ComputeIndex()
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(GenerateMetricsForProject());
+            var metricNode = xmlDoc.SelectSingleNode($"//Method[contains(@Name, '{new MISettings().TargetMethodName}')]/Metrics/Metric[@Name = 'MaintainabilityIndex']");
+            var miValue = metricNode.Attributes["Value"].Value;
+            if (!int.TryParse(miValue, out int mi))
+            {
+                throw new ArgumentException("Can't parse value returned from ms metrics");
+            }
+
+            return mi;
+        }
+
+        private static string GenerateMetricsForProject()
         {
             var process = new Process()
             {
@@ -19,16 +40,15 @@ namespace GenSharp
                 },
                 EnableRaisingEvents = true
             };
+            var xmlBuilder = new StringBuilder();
             process.OutputDataReceived += (sender, e) =>
             {
-                var xmlDoc = new XmlDocument();
-                xmlDoc.LoadXml(e.Data);
-                var metricNode = xmlDoc.SelectSingleNode($"number(//Method[contains(@Name, '{new MISettings().TargetMethodName}')]/Metrics/Metric[@Name = 'MaintainabilityIndex'])");
-                var miValue = metricNode.Attributes["Value"].Value;
+                xmlBuilder.AppendLine(e.Data);
             };
             process.Start();
             process.BeginOutputReadLine();
             process.WaitForExit();
+            return xmlBuilder.ToString();
         }
     }
 }
