34175168a32d80243afe3c885ac48c1acaa5432a
Damian Ubowski <sverda300@gmail.com>
2020-01-04 14:47:54 +0100

    [ExtractMI] Setting for MI tool path

---
+++ AppSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Configuration;
+
+namespace GenSharp
+{
+    class AppSettings
+    {
+        private const string _miKey = "MaintainabilityIndexToolPath";
+
+        public string MetricsExecutablePath { get; }
+
+        public AppSettings()
+        {
+            MetricsExecutablePath = ConfigurationManager.AppSettings[_miKey];
+            if (string.IsNullOrEmpty(MetricsExecutablePath))
+            {
+                throw new ArgumentNullException(nameof(MetricsExecutablePath));
+            }
+        }
+    }
+}
