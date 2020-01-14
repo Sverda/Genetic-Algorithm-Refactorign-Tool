c52bd826a4e9b70bc15365a072c6d760db6a1a5d
Damian Ubowski <sverda300@gmail.com>
2020-01-14 19:45:23 +0100

    [FirstRefactoring] Add more unit tests & Change tests' naming

---
+++ src/GenSharp.Refactorings/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace GenSharp.Refactorings
+{
+    public static class StringExtensions
+    {
+        public static void GetSemanticModel(this string source, out SyntaxTree tree, out SemanticModel model)
+        {
+            tree = CSharpSyntaxTree.ParseText(source);
+            var compilation = CSharpCompilation
+                .Create("GenSharp.Tests.Refactorings")
+                .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
+                .AddSyntaxTrees(tree);
+            model = compilation.GetSemanticModel(tree);
+        }
+    }
+}
